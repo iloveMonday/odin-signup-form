e10744f6e465f64075eb04aaaef59182385b3c2a
IloveMonday <iloveyouchandlerbing@gmail.com>
2024-03-11 22:44:25 -0400

    form validations working

---
+++ script.js
@@ -0,0 +1,18 @@
+let username = document.getElementById("firstname");
+let password = document.getElementById("password");
+let conpassword = document.getElementById("conpassword");
+
+
+function check(){
+    let message = document.getElementById('message');
+    let submit = document.getElementById("submit");
+    if(document.getElementById("password").value === document.getElementById("conpassword").value){
+            message.style.color = 'green';
+            message.innerHTML = '☑ you got it dude';
+            submit.disabled = false;
+          } else {
+            message.style.color = 'red';
+            message.innerHTML = '☒ not a match, try again!'; 
+            submit.disabled = true;
+          }
+  }
